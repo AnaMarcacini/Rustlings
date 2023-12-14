e38ab4e217f6a4f607664406c6fdaafd34c93ff8
Ana_D <ana.marcacini@maua.br>
2023-12-14 09:11:23 -0300

    Aula 2 completa

--- exercises/00_intro/intro2.rs
+++ exercises/00_intro/intro2.rs
@@ -7,9 +7,5 @@
 
 
 fn main() {
-<<<<<<< HEAD:exercises/intro/intro2.rs
     println!("Hello Ana!");
-=======
-    printline!("Hello there!")
->>>>>>> 88b583f2bbf4d7a668db6d83964af5470c93e516:exercises/00_intro/intro2.rs
 }
